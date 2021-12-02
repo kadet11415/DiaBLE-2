4c052c1b191cae4711a9c0d1987173c1d7c52e57
Guido Soranzio <7220550+gui-dos@users.noreply.github.com>
2021-12-02 11:27:23 +0100

    Libre 3: state = `patchInfo[16] -1` (#7)

--- DiaBLE/Libre3.swift
+++ DiaBLE/Libre3.swift
@@ -159,7 +159,10 @@ class Libre3: Libre2 {
     func parsePatchInfo() {
         if patchInfo.count == 28 {
             log("Libre 3: patch info: \(patchInfo.hexBytes), CRC: \(Data(patchInfo.suffix(2).reversed()).hex), computed CRC: \(patchInfo[2...25].crc16.hex)")
-            log("Libre 3: state: \(patchInfo[16])")
+            // TODO
+            let sensorState = patchInfo[16]
+            state = SensorState(rawValue: sensorState - 1)!
+            log("Libre 3: state: \(state.description.lowercased()) (0x\(sensorState.hex))")
         }
     }
 
